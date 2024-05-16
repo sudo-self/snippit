eb6c089123ce686744bdbb87aa8901a42dad2d1e
Sudo-Self <sudo-self@proton.me>
2024-05-15 19:59:42 -0600

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -129,8 +129,8 @@ const firebaseConfig = {
 </script>
 <main>
 <script async defer src="https://buttons.github.io/buttons.js"></script>
-<h1>Welcome to Snippitz!<a class="github-button" href="https://github.com/sudo-self/snippitz" data-color-scheme="no-preference: light_high_contrast; light: light_high_contrast; dark: light_high_contrast;" data-icon="octicon-star" data-size="large" aria-label="Star sudo-self/snippitz on GitHub">Star</a></h1> 
-<button on:click="{() => document.body.classList.toggle('light-theme')}">Toggle Theme</button>
+<h1>Welcome to Snippitz!</h1> 
+<a class="github-button" href="https://github.com/sudo-self/snippitz" data-color-scheme="no-preference: light_high_contrast; light: light_high_contrast; dark: light_high_contrast;" data-icon="octicon-star" data-size="large" aria-label="Star sudo-self/snippitz on GitHub">Star</a>
    <section>
     <h2><code>SHARE. YOUR. CODE.</code></h2>
     <div>
